0da31c2a4dfe0d74ee38c4fd985567a243b1f7c2
Kevin VanderLugt <kevin.vanderlugt@gmail.com>
2013-09-22 13:30:18 -0700

    Rendering out the header and fotoer as partials

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,12 @@ class ApplicationController < ActionController::Base
     def sign_in(user)
       session[:user_id] = user.id if user
     end
+
+    def current_user
+      @user ||= User.find(session[:user_id]) if session[:user_id]
+    end
+
+    def current_user?
+      current_user.present?
+    end
 end
