3db50c2359874cb2877cc6390eb7d00c95c3aef6
Kevin VanderLugt <kevin.vanderlugt@gmail.com>
2013-09-22 17:11:44 -0700

    Require email to be unique for accounts

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130922225011) do
+ActiveRecord::Schema.define(version: 20130923000942) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,5 +33,7 @@ ActiveRecord::Schema.define(version: 20130922225011) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
 
 end
