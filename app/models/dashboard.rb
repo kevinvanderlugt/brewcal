61a15bb6df31d9ca6197fb86f884b0e4b6d4e00b
Kevin VanderLugt <kevin.vanderlugt@gmail.com>
2013-09-23 22:39:24 -0700

    Add styling to dashboard

--- app/models/dashboard.rb
+++ app/models/dashboard.rb
@@ -2,6 +2,6 @@ class Dashboard < ActiveRecord::Base
   belongs_to :user
 
   def brew_statuses
-    %i(planning brewing fermenting conditioning on-tap)
+    %i(planning fermenting conditioning on-tap)
   end
 end
